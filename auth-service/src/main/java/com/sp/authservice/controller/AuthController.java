57fc96c0b27aa7bd2684ea8d896ee635d28215b0
Kivindu Rajamanukula <manusha.rajamanukula@gmail.com>
2026-05-29 01:23:58 +0530

    feat(auth-service): implement cascade delete via Kafka events for user

--- auth-service/src/main/java/com/sp/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/sp/authservice/controller/AuthController.java
@@ -5,10 +5,14 @@ import com.sp.authservice.dto.LoginResponseDTO;
 import com.sp.authservice.dto.RegisterRequestDTO;
 import com.sp.authservice.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -16,6 +20,7 @@ import java.util.UUID;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
     private final UserService userService;
 
     public AuthController(UserService userService) {
@@ -46,4 +51,29 @@ public class AuthController {
         userService.verifyEmail(token);
         return ResponseEntity.ok("Email verified successfully! You can now login.");
     }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        UUID loggedInUserId = getAuthenticatedUserId();
+
+        boolean isAdmin = auth.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+
+        userService.deleteUser(id, loggedInUserId, isAdmin);
+        return ResponseEntity.noContent().build();
+    }
+
+    private UUID getAuthenticatedUserId() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getDetails() == null) {
+            log.error("Authentication or details (userId) are missing from the security context");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User identity not verified");
+        }
+
+        return UUID.fromString(authentication.getDetails().toString());
+    }
+
 }
--- auth-service/src/main/java/com/sp/authservice/service/UserService.java
+++ auth-service/src/main/java/com/sp/authservice/service/UserService.java
@@ -4,12 +4,17 @@ import com.sp.authservice.dto.LoginRequestDTO;
 import com.sp.authservice.dto.LoginResponseDTO;
 import com.sp.authservice.dto.RegisterRequestDTO;
 import com.sp.authservice.exception.*;
+import com.sp.authservice.kafka.UserKafkaProducer;
 import com.sp.authservice.mapper.UserMapper;
 import com.sp.authservice.model.User;
 import com.sp.authservice.repository.UserRepository;
 import com.sp.authservice.util.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -18,19 +23,26 @@ import java.util.UUID;
 @Service
 public class UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
     private final EmailService emailService;
+    private final UserKafkaProducer userKafkaProducer;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder,
-            JwtUtil jwtUtil, EmailService emailService) {
+    public UserService(UserRepository userRepository,
+                       UserMapper userMapper,
+                       PasswordEncoder passwordEncoder,
+                       JwtUtil jwtUtil,
+                       EmailService emailService,
+                       UserKafkaProducer userKafkaProducer) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
         this.emailService = emailService;
+        this.userKafkaProducer = userKafkaProducer;
     }
 
     public void register(RegisterRequestDTO registerRequestDTO) {
@@ -97,5 +109,33 @@ public class UserService {
         user.setVerificationToken(null);
         user.setTokenExpiresAt(null);
         userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteUser(UUID id, UUID loggedInUserId, boolean isAdmin) {
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new UserNotFoundException("User not found"));
+
+        validateAccess(user, loggedInUserId, isAdmin);
+
+        userRepository.delete(user);
+
+        try {
+            userKafkaProducer.sendUserDeleteEvent(id);
+            log.info("User {} deleted and USER_DELETED event published", id);
+        } catch (Exception e) {
+            log.error("USER_DELETED Kafka event failed for userId {}: {}", id, e.getMessage());
+        }
+
+    }
+
+    private void validateAccess(User user, UUID loggedInUserId, boolean isAdmin) {
+        // Allow if user is Admin OR if user is the Owner
+        if (isAdmin || user.getId().equals(loggedInUserId)) {
+            return; // Access granted
+        }
+
+        log.error("Unauthorized access attempt by user {}", loggedInUserId);
+        throw new AccessDeniedException("Access Denied: You do not have permission to delete this.");
     }
 }
