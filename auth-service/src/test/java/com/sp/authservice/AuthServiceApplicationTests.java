5dddfadd17128895012a0fc1bc25cca07e7143e0
Kivindu Rajamanukula <manusha.rajamanukula@gmail.com>
2026-07-19 10:21:41 +0530

    Add test profile with H2 for auth-service CI

--- auth-service/src/test/java/com/sp/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/sp/authservice/AuthServiceApplicationTests.java
@@ -2,9 +2,11 @@ package com.sp.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+@ActiveProfiles("test")
+class  AuthServiceApplicationTests {
 
     @Test
     void contextLoads() {
