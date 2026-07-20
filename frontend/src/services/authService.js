a8f372985d90fd0b562b26c3b0afe628e2e1df3d
Kivindu Rajamanukula <manusha.rajamanukula@gmail.com>
2026-07-20 10:55:19 +0530

    Add authService.js with login and register API calls

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const login = async (email, password) => {
+    const response = await axios.post(`${API_URL}/auth/login`, {
+        email,
+        password
+    });
+    return response.data;
+};
+
+export const register = async (firstName, lastName, email, password, confirmPassword) => {
+    const response = await axios.post(`${API_URL}/auth/register`, {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword
+    });
+    return response.data;
+};
